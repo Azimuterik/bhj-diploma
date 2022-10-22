0057e7d0be2080b193654e235f1e57303a7fea95
azimuterik <azimuterik@gmail.com>
2022-10-22 14:57:05 +0300

    createRequest made

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,45 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+ const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    
+    let formData = null;
+    let url = options.url;
 
+    if (options.data) {
+        if (options.method === 'GET') {
+            url += '?' + Object.entries(options.data).map(
+                entry => entry.map(encodeURIComponent).join('=')
+            ).join('&');
+        } else {
+            formData = new FormData();
+            Object.entries(options.data).forEach(v => formData.append(...v));
+        }
+    }
+
+
+    
+    if (options.callback) {
+        xhr.onload = () => {
+            let err = null;
+            let resp = null;
+
+            try { 
+                if (xhr.response?.success) {
+                    resp = xhr.response;
+                } else {
+                    err = xhr.response;
+                }
+            } catch (error) {
+                err = error;
+            }
+
+            options.callback(err, resp);
+        };
+    }
+    
+    xhr.open(options.method, url);
+    xhr.send(formData);
 };
