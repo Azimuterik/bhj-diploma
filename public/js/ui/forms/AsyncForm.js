4f3c5400bd9ccad978c1aab15e223df0cd9acac4
azimuterik <azimuterik@gmail.com>
2022-10-22 21:00:14 +0300

    AsyncForm made

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -6,6 +6,8 @@
  * для последующей обработки
  * */
 class AsyncForm {
+
+  
   /**
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
@@ -13,6 +15,12 @@ class AsyncForm {
    * через registerEvents()
    * */
   constructor(element) {
+    if (!element) {
+      throw new Error('Элемент не существует');
+    }
+
+    this.element = element;
+    this.registerEvents();
 
   }
 
@@ -21,7 +29,10 @@ class AsyncForm {
    * вызывает метод submit()
    * */
   registerEvents() {
-
+    this.element.onSubmit = e => {
+      e.preventDefault();
+      this.submit();
+    }
   }
 
   /**
@@ -33,6 +44,8 @@ class AsyncForm {
    * */
   getData() {
 
+    const formData = new FormData(this.element);
+    return Object.fromEntries(formData.entries());
   }
 
   onSubmit(options){
@@ -44,6 +57,6 @@ class AsyncForm {
    * данные, полученные из метода getData()
    * */
   submit() {
-
+    this.onSubmit(this.getData());
   }
 }
